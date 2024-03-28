05e301f231208ab87a1d03371baa26e08b73ec46
TylerLaBree <tylerlabree@github.com>
2024-03-28 16:15:07 -0500

    fix: virtual environment and dunesw dependency conflict

--- make-venv.sh
+++ make-venv.sh
@@ -2,6 +2,10 @@
 # Tyler LaBree
 # Northern Illinois University
 
+source /cvmfs/dune.opensciencegrid.org/products/dune/setup_dune.sh
+setup dunesw v09_78_03d01 -q e20:prof
+setup dune_plot_style v01_00
+
 python3 -m venv venv
 source venv/bin/activate
 python3 -m pip install --upgrade pip
