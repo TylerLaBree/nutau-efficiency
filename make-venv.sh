be98cf36ac860c0d668b17ad01c00d64e0fba51b
TylerLaBree <tylerlabree@github.com>
2024-04-05 11:54:55 -0500

    feat: add dune-plot-style to virtual environment

--- make-venv.sh
+++ make-venv.sh
@@ -4,10 +4,15 @@
 
 source /cvmfs/dune.opensciencegrid.org/products/dune/setup_dune.sh
 setup dunesw v09_78_03d01 -q e20:prof
-setup dune_plot_style v01_00
 
 python3 -m venv venv
 source venv/bin/activate
-python3 -m pip install --upgrade pip
-python3 -m pip install pyroot jupyterlab urllib3==1.26.6
+venv/bin/python3.9 -m pip install --upgrade pip
+venv/bin/python3.9 -m pip install pyroot jupyterlab urllib3==1.26.6 matplotlib scipy
 
+export DUNE_PLOT_STYLE_LATEST_TAG=`curl --silent "https://api.github.com/repos/DUNE/dune_plot_style/releases" | jq -r 'map(select(.prerelease == false)) | first | .tag_name'`
+wget --no-check-certificate https://github.com/DUNE/dune_plot_style/archive/refs/tags/${DUNE_PLOT_STYLE_LATEST_TAG}.tar.gz -O dune_plot_style.tar.gz
+tar -xvzf dune_plot_style.tar.gz
+cd dune_plot_style-01_00
+../venv/bin/python3.9 -m pip install .
+cd ..
